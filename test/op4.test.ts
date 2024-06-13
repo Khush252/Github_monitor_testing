b41da532e15b684b70a72fd0b8fe0ec5b61ca752
Saugat Kannojia <saugatk@cse.iitk.ac.in>
2024-06-13 18:48:11 +0530

    commit 1

---
+++ test/op4.test.ts
@@ -0,0 +1,20 @@
+fasasv
+async function async function 
+dsdaf
+asd
+a
+SVGAElement
+awfaqw
+encodeURI(params:
+     AudioScheduledSourceNodeA
+
+) {
+    nameQS DSVDMUJASD
+    `QWD`
+}(Q`DN NH`:SVGDefsElement`WD AMS `) {
+    QWDMJW
+DOMExceptionF F[W`F`
+        FEWVQW
+        WD V    QWD     
+]
+}
